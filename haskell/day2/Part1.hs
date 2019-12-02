5e43e4bc07fc5fd0998590ea649884af6127f3b4
Tommy Schaefer <tommy.schaefer@teecom.com>
2019-12-02 10:36:55 -0600

    Refactor day 2 part 1 in Haskell

--- haskell/day2/Part1.hs
+++ haskell/day2/Part1.hs
@@ -1,39 +1,52 @@
 module Part1 where
 
-data Instruction = Add Int Int Int | Multiply Int Int Int| Stop deriving Show
+data Instruction
+    = Add Int Int Int
+    | Multiply Int Int Int
+    | Stop
+    deriving (Show)
 
 wordsBy :: Char -> String -> [String]
 wordsBy sep str = case dropWhile matchesSep str of
     "" -> []
-    s -> w : wordsBy sep s' where (w, s') = break matchesSep s
-    where matchesSep = (== sep)
+    s  -> w : wordsBy sep s' where (w, s') = break matchesSep s
+  where
+    matchesSep = (== sep)
 
-update :: (Int -> Int -> Int) -> Int -> Int -> Int -> [Int] -> [Int]
-update f a b c xs = head ++ [v] ++ tail
-    where v = f (xs !! a) (xs !! b)
-          (head,_:tail) = splitAt c xs
+updateWith :: Int -> ([a] -> a) -> [a] -> [a]
+updateWith i f vs = head ++ f vs:tail where (head, _:tail) = splitAt i vs
 
-opcode :: Int -> [Int] -> (Instruction, [Int])
-opcode idx list = case list' of
-    (1:a:b:c:_) -> (Add a b c, list)
-    (2:a:b:c:_) -> (Multiply a b c, list)
-    (99:_) -> (Stop, list)
-    where (_, list') = splitAt idx list
+addressValues :: Int -> Int -> [Int] -> (Int, Int)
+addressValues pa pb memory = (memory !! pa, memory !! pb)
+
+applyToAddresses :: (Int -> Int -> Int) -> Int -> Int -> [Int] -> Int
+applyToAddresses f pa pb = uncurry f . addressValues pa pb
+
+applyInstruction :: Instruction -> [Int] -> [Int]
+applyInstruction (Add pa pb pc)      = updateWith pc $ applyToAddresses (+) pa pb
+applyInstruction (Multiply pa pb pc) = updateWith pc $ applyToAddresses (*) pa pb
+
+instructionAt :: Int -> [Int] -> Instruction
+instructionAt address memory = case list' of
+    (1:a:b:c:_) -> Add a b c
+    (2:a:b:c:_) -> Multiply a b c
+    (99:_)      -> Stop
+  where
+    (_, list') = splitAt address memory
 
 calculate :: Int -> [Int] -> [Int]
-calculate idx xs = case opcode idx xs of
-    (Add a b c, list) -> calculate (idx + 4) $ update (+) a b c list
-    (Multiply a b c, list) -> calculate (idx + 4) $ update (*) a b c list
-    (Stop, list) -> list
+calculate address memory = case instructionAt address memory of
+    Stop -> memory
+    i    -> calculate (address + 4) $ applyInstruction i memory
 
-restore :: [Int] -> [Int]
-restore (a:_:_:xs) = (a:12:2:xs)
+restore :: Int -> Int -> [Int] -> [Int]
+restore n v (a:_:_:xs) = a:n:v:xs
 
 parseInput :: String -> [Int]
 parseInput = map read . wordsBy ','
 
 answer :: String -> Int
-answer = head . calculate 0 . restore . parseInput
+answer = head . calculate 0 . restore 12 2 . parseInput
 
 main :: IO ()
 main = interact $ show . answer
