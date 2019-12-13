d3048d97e1dbfcc7d703e75077bc36f9568c898b
Tommy Schaefer <tommy.schaefer@teecom.com>
2019-12-13 11:39:50 -0600

    Clean up day 2 a bit

--- haskell/day2/Part1.hs
+++ haskell/day2/Part1.hs
@@ -2,6 +2,8 @@ module Part1 where
 
 import           Data.List.Split (splitOn)
 
+type Program = [Int]
+
 data Instruction
     = Add Int Int Int
     | Multiply Int Int Int
@@ -11,37 +13,40 @@ data Instruction
 updateWith :: Int -> ([a] -> a) -> [a] -> [a]
 updateWith i f vs = head ++ f vs:tail where (head, _:tail) = splitAt i vs
 
-addressValues :: Int -> Int -> [Int] -> (Int, Int)
-addressValues pa pb memory = (memory !! pa, memory !! pb)
+addressValues :: Int -> Int -> Program -> (Int, Int)
+addressValues pa pb prog = (prog !! pa, prog !! pb)
 
-applyToAddresses :: (Int -> Int -> Int) -> Int -> Int -> [Int] -> Int
+applyToAddresses :: (Int -> Int -> Int) -> Int -> Int -> Program -> Int
 applyToAddresses f pa pb = uncurry f . addressValues pa pb
 
-applyInstruction :: Instruction -> [Int] -> [Int]
+applyInstruction :: Instruction -> Program -> Program
 applyInstruction (Add pa pb pc)      = updateWith pc $ applyToAddresses (+) pa pb
 applyInstruction (Multiply pa pb pc) = updateWith pc $ applyToAddresses (*) pa pb
 
-instructionAt :: Int -> [Int] -> Instruction
-instructionAt address memory = case list' of
+instructionAt :: Int -> Program -> Instruction
+instructionAt addr prog = case list' of
     (1:a:b:c:_) -> Add a b c
     (2:a:b:c:_) -> Multiply a b c
     (99:_)      -> Stop
   where
-    (_, list') = splitAt address memory
+    (_, list') = splitAt addr prog
 
-calculate :: Int -> [Int] -> [Int]
-calculate address memory = case instructionAt address memory of
-    Stop -> memory
-    i    -> calculate (address + 4) $ applyInstruction i memory
+exec :: Int -> Program -> Program
+exec addr prog = case instructionAt addr prog of
+    Stop -> prog
+    i    -> exec (addr + 4) $ applyInstruction i prog
 
-restore :: Int -> Int -> [Int] -> [Int]
+run :: Program -> Program
+run = exec 0
+
+restore :: Int -> Int -> Program -> Program
 restore n v (a:_:_:xs) = a:n:v:xs
 
-parseInput :: String -> [Int]
-parseInput = map read . splitOn ","
+parseProg :: String -> Program
+parseProg = map read . splitOn ","
 
 answer :: String -> Int
-answer = head . calculate 0 . restore 12 2 . parseInput
+answer = head . run . restore 12 2 . parseProg
 
 main :: IO ()
 main = interact $ show . answer
--- haskell/day2/Part2.hs
+++ haskell/day2/Part2.hs
@@ -4,6 +4,8 @@ import           Data.List       (find)
 import           Data.List.Split (splitOn)
 import           Data.Maybe      (fromMaybe)
 
+type Program = [Int]
+
 data Instruction
     = Add Int Int Int
     | Multiply Int Int Int
@@ -16,43 +18,46 @@ combinations as bs = [(a, b) | a <- as, b <- bs]
 updateWith :: Int -> ([a] -> a) -> [a] -> [a]
 updateWith i f vs = head ++ f vs:tail where (head, _:tail) = splitAt i vs
 
-addressValues :: Int -> Int -> [Int] -> (Int, Int)
-addressValues pa pb memory = (memory !! pa, memory !! pb)
+addressValues :: Int -> Int -> Program -> (Int, Int)
+addressValues pa pb prog = (prog !! pa, prog !! pb)
 
-applyToAddresses :: (Int -> Int -> Int) -> Int -> Int -> [Int] -> Int
+applyToAddresses :: (Int -> Int -> Int) -> Int -> Int -> Program -> Int
 applyToAddresses f pa pb = uncurry f . addressValues pa pb
 
-applyInstruction :: Instruction -> [Int] -> [Int]
+applyInstruction :: Instruction -> Program -> Program
 applyInstruction (Add pa pb pc)      = updateWith pc $ applyToAddresses (+) pa pb
 applyInstruction (Multiply pa pb pc) = updateWith pc $ applyToAddresses (*) pa pb
 
-instructionAt :: Int -> [Int] -> Instruction
-instructionAt address memory = case list' of
+instructionAt :: Int -> Program -> Instruction
+instructionAt addr prog = case list' of
     (1:a:b:c:_) -> Add a b c
     (2:a:b:c:_) -> Multiply a b c
     (99:_)      -> Stop
   where
-    (_, list') = splitAt address memory
+    (_, list') = splitAt addr prog
 
-calculate :: Int -> [Int] -> [Int]
-calculate address memory = case instructionAt address memory of
-    Stop -> memory
-    i    -> calculate (address + 4) $ applyInstruction i memory
+exec :: Int -> Program -> Program
+exec addr prog = case instructionAt addr prog of
+    Stop -> prog
+    i    -> exec (addr + 4) $ applyInstruction i prog
 
-restore :: Int -> Int -> [Int] -> [Int]
+run :: Program -> Program
+run = exec 0
+
+restore :: Int -> Int -> Program -> Program
 restore n v (a:_:_:xs) = a:n:v:xs
 
-parseInput :: String -> [Int]
-parseInput = map read . splitOn ","
+parseProg :: String -> Program
+parseProg = map read . splitOn ","
 
-compute :: Int -> Int -> String -> Int
-compute n v = head . calculate 0 . restore n v . parseInput
+compute :: (Int, Int) -> String -> Int
+compute (n, v) = head . run . restore n v . parseProg
 
-isTarget :: String -> (Int, Int) -> Bool
-isTarget s (n, v) = 19690720 == compute n v s
+computesValue :: Int -> String -> (Int, Int) -> Bool
+computesValue v s nv = v == compute nv s
 
 validSolution :: String -> Maybe (Int, Int)
-validSolution s = find (isTarget s) $ combinations [0..99] [0..99]
+validSolution s = find (computesValue 19690720 s) $ combinations [0..99] [0..99]
 
 answer :: String -> Maybe Int
 answer s = format <$> validSolution s
